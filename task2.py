37d5bada34fb98ff16dfd31b380485a171ae51d8
tuisejul <166166403+tuisejul@users.noreply.github.com>
2024-05-17 12:52:52 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,4 +1,15 @@
-price = int(input())
-count = int(input())
-money = int(input())
-print(money - (price*count))
+price = int(input('Введите цену'))
+count = float(input('Введите количество подукта'))
+money = int(input('Введите внесенные средства'))
+print(money - (price*count)) #Задачи 1-2
+
+name = str(input('Введите название товара'))
+weight = float(input('Введите вес товара'))
+price1 = float(input('Введите цену товара'))
+S = price1*weight
+Cash = float(input('Введите внесенные средства'))
+A = Cash - S
+print('Чек', name, 'вес', weight, 'цена', price1,
+      'Итого:', S,
+      'Внесено:', Cash,
+      'Сдача:', A ) #Задача 3
